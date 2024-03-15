7f22722c906d8c63e96ce7044931dda2f0163ffc
Hojoon Kim <elddy0948@gmail.com>
2024-03-16 01:44:52 +0900

    feat: add GPIO base addresses

--- USART_practice/USARTUSART/Inc/Drivers/usart.h
+++ USART_practice/USARTUSART/Inc/Drivers/usart.h
@@ -58,5 +58,24 @@ typedef struct
 #define USART_HW_FLOW_CTRL_RTS		2
 #define USART_HW_CTRL_CTRL_CTS_RTS	3
 
+void USART_PeriClockControl(USART_RegDef_t *pUSARTx, uint8_t EnorDi);
+
+void USART_Init(USART_Handle_t *pUSARTHandle);
+void USART_DeInit(USART_RegDef_t *pUSARTx);
+
+void USART_SendData(USART_RegDef_t *pUSARTx, uint8_t *pTxBuffer, uint32_t Len);
+void USART_ReceiveData(USART_RegDef_t *pUSARTx, uint8_t *pRxBuffer, uint32_t Len);
+uint8_t USART_SendDataIT(USART_Handle_t *pUSARTHandle, uint8_t *pTxBuffer, uint32_t Len);
+uint8_t USART_ReceiveDataIT(USART_Handle_t *pUSARTHandle, uint8_t *pRxBuffer, uint32_t Len);
+
+void USART_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi);
+void USART_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);
+void USART_IRQHandling(USART_Handle_t *pHandle);
+
+void USART_PeripheralControl(USART_RegDef_t *pUSARTx, uint8_t EnorDi);
+uint8_t USART_GetFlagStatus(USART_RegDef_t *pUSARTx, uint32_t FlagName);
+void USART_ClearFlag(USART_RegDef_t *pUSARTx, uint16_t StatusFlagName);
+
+void USART_ApplicationEventCallback(USART_Handle_t *pUSARTHandle, uint8_t AppEv);
 
 #endif /* DRIVERS_USART_H_ */
