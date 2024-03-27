7dac1b3e000b5842b3ebd4005b33d58c8ddca395
Hojoon Kim <elddy0948@gmail.com>
2024-03-27 11:39:44 +0900

    feat: add button toggle code

--- USART_practice/USARTUSART/Src/led_toggle.c
+++ USART_practice/USARTUSART/Src/led_toggle.c
@@ -8,16 +8,21 @@
 
 #include "Drivers/stm32f767xx.h"
 
+#define HIGH 1
+#define BTN_PRESSED HIGH
+
 void delay(void)
 {
-	for(uint32_t i = 0; i < 500000; ++i);
+	for(uint32_t i = 0; i < 500000 / 2; ++i);
 }
 
 int main(void)
 {
-	// PB7
+	// PB7 - LED
+	// PC13 - BUTTON
 
 	GPIO_Handle_t LED_GPIO;
+	GPIO_Handle_t Button_GPIO;
 
 	LED_GPIO.pGPIOx = GPIOB;
 	LED_GPIO.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NUMBER_7;
@@ -26,14 +31,25 @@ int main(void)
 	LED_GPIO.GPIO_PinConfig.GPIO_PinOPType = GPIO_OUTPUT_TYPE_PP;
 	LED_GPIO.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPD_NO_PUPD;
 
+	Button_GPIO.pGPIOx = GPIOC;
+	Button_GPIO.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NUMBER_13;
+	Button_GPIO.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_INPUT;
+	Button_GPIO.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	Button_GPIO.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPD_NO_PUPD;
+
 	GPIO_PCLKControl(GPIOB, ENABLE);
+	GPIO_PCLKControl(GPIOC, ENABLE);
 
 	GPIO_Init(&LED_GPIO);
+	GPIO_Init(&Button_GPIO);
 
 	while(1)
 	{
-		GPIO_ToggleOutputPin(GPIOB, GPIO_PIN_NUMBER_7);
-		delay();
+		if(GPIO_ReadFromInputPin(GPIOC, GPIO_PIN_NUMBER_13) == BTN_PRESSED)
+		{
+			delay();
+			GPIO_ToggleOutputPin(GPIOB, GPIO_PIN_NUMBER_7);
+		}
 	}
 
 	return 0;
